a4b8f25651970b37d0b04bcf565f65d7f8b29604
Farooq Mahmud <Farooq.Mahmud@premera.com>
2018-09-27 17:47:13 -0700

    Added a test.

--- stupidlittlewebsite/index.js
+++ stupidlittlewebsite/index.js
@@ -1,12 +1,15 @@
 const Koa = require("koa");
 
-const app = new Koa();
+const app = module.exports = new Koa();
 
 app.use(function *() {
     this.body = "Koa says hi!";
 });
 
 const port = process.env.PORT || (process.argv[2] || 4567);
-app.listen(port);
+
+if (!module.parent) {
+    app.listen(port);
+}
 
 console.log(`Application started and listening on port ${port}`);
